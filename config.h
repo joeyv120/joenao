44050f6628dab9f5a08ebb847ce11667636699d7
joeyv120 <joeyv120@outlook.com>
2021-12-01 10:20:12 -0500

    removed TAPPING_FORCE_HOLD... did the opposite

--- config.h
+++ config.h
@@ -53,4 +53,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Tap-Hold Configuration Options */
 #define TAPPING_TERM 200
 #define IGNORE_MOD_TAP_INTERRUPT
-#define TAPPING_FORCE_HOLD
